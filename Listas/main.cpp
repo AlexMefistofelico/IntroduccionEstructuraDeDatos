5ecf20d9405a4828003cf21e6e69692b96469d78
Alex <alex.mefistofelico@gmail.com>
2019-09-01 20:00:20 -0400

    Plantilla main

---
+++ Listas/main.cpp
@@ -0,0 +1,29 @@
+#include<algorithm>
+#include<cstdio>
+#include<cstdlib>
+#include<iostream>
+#include<stdexcept>
+using namespace std;
+/**
+    @AlexAriel
+    $WEEKDAY_UTC
+    $NOW_L_UTC
+    NOTAS.-
+
+*/
+|[maybe_unused]] int main(int argc,char *argv[],char **env) try {
+    freopen("in.txt","r",stdin);
+
+    printf(  "Prueba...\n");
+    puts(    "Prueba...");
+    cout<<   "Prueba..."<<endl;
+
+
+
+
+
+    return EXIT_SUCCESS;
+}catch(exception& e){
+    cerr<<"ERROR...!!!: "<<endl<<e.what()<<endl;
+    return EXIT_FAILURE;
+}
